c95f18764cc3e7185b4f751ddd7c7c7f5797ffa2
Rachel Shorey <rshorey@gmail.com>
2015-03-16 23:21:47 -0400

    more trash added by Eliza

--- trashtalk.py
+++ trashtalk.py
@@ -1,11 +1,14 @@
 import random
 
 nouns = ["team","bracket","code","github account",
-        "database","algorithm"]
+        "database","algorithm","free throw percent",
+        "point guard","mascot","t-shirt cannon"]
 
 adjectives = ["stinky","small","hackable",
                 "dastardly","Machiavellian",
-                "irresponsible"]
+                "irresponsible","serpentine",
+                "deflated","a loser","going down",
+                "doomed","flat-footed"]
 
 noun = random.choice(nouns)
 adj = random.choice(adjectives)
