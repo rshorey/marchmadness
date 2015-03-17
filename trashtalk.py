bb07d0fbee56d1dde67fc328bac31623a9268bd4
Rachel Shorey <rshorey@gmail.com>
2015-03-17 10:23:20 -0400

    bracket generated, trash talked

--- trashtalk.py
+++ trashtalk.py
@@ -2,13 +2,15 @@ import random
 
 nouns = ["team","bracket","code","github account",
         "database","algorithm","free throw percent",
-        "point guard","mascot","t-shirt cannon"]
+        "point guard","mascot","t-shirt cannon","coach"]
 
 adjectives = ["stinky","small","hackable",
                 "dastardly","Machiavellian",
                 "irresponsible","serpentine",
                 "deflated","a loser","going down",
-                "doomed","flat-footed"]
+                "doomed","flat-footed","pseudorandom",
+                "outside the margin of error","full of eels",
+                "a hamster"]
 
 noun = random.choice(nouns)
 adj = random.choice(adjectives)
